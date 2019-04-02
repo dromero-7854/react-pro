20202259a750f6ba237ad7a4b2a7981334f801ed
Dante Romero <dante.romero@tecso.coop>
2019-04-02 15:53:15 -0300

    server side rendering

--- src/server/html.js
+++ src/server/html.js
@@ -2,22 +2,21 @@ export default function html(options) {
   const {
     app = 'main',
     title = 'CodeJobs',
-    stylesheet = '/css/style.css'//,
-    //markup
+    stylesheet = '/css/style.css',
+    markup
   } = options;
-
   return `
     <!DOCTYPE html>
     <html>
       <head>
-        <meta charset="utf-8" />
-        <title>${title}<title/>
+        <meta charset="utf-8">
+        <title>${title}</title>
         <link rel="stylesheet" href="${stylesheet}" />
       </head>
       <body>
-        <div id="root"></div>
-        <script src="/app/${app}.blunde.js"></script>
-      <body/>
+        <div id="root">${markup}</div>
+        <script src="/app/${app}.bundle.js"></script>
+      </body>
     </html>
   `;
 }
